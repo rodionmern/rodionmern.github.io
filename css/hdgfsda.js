539bc94189bb7df09f0a4a649d96f84b0fb97b0a
rodeosmith <rodeossmith@gmail.com>
2023-07-14 12:36:11 +0300

    Create hdgfsda.js

---
+++ css/hdgfsda.js
@@ -0,0 +1,7 @@
+5 > 4 true | true
+"ананас" > "яблоко" true | false
+"2" > "12" false | true
+undefined == null true | true
+undefined === null false | false
+null == "\n0\n"  true | false
+null === +"\n0\n" false | false
