55c31dece58dc8d512e65ce7ba17d8bc6691ddd4
rodeosmith <rodeossmith@gmail.com>
2023-07-14 17:20:51 +0300

    reload header, footer | adaptive

--- css/hdgfsda.js
+++
@@ -1,7 +0,0 @@
-5 > 4 true | true
-"ананас" > "яблоко" true | false
-"2" > "12" false | true
-undefined == null true | true
-undefined === null false | false
-null == "\n0\n"  true | false
-null === +"\n0\n" false | false
